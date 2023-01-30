31ac8568c2a92654a02a2de3aca76b666636d67a
StoatPower <mattcaldwell84@gmail.com>
2023-01-30 12:41:30 -0500

    added tests for TreeNode.new

--- rose_tree/test/rose_tree/tree_node_test.exs
+++ rose_tree/test/rose_tree/tree_node_test.exs
@@ -4,7 +4,7 @@ defmodule RoseTree.TreeNodeTest do
 
   doctest RoseTree.TreeNode
 
-  @bad_values [
+  @bad_trees [
     {%{term: "parent", children: []}, 0},
     {true, 1},
     {false, 2},
@@ -12,7 +12,22 @@ defmodule RoseTree.TreeNodeTest do
     {"a word", 4},
     {6.4, 5},
     {:a_thing, 6},
-    {%TreeNode{term: "parent", children: "bad_child"}, 7}
+    {[1,2,3], 7},
+    {{1,2}, 8},
+    {%TreeNode{term: "parent", children: "bad_child"}, 9},
+    {nil, 10}
+  ]
+
+  @node_values [
+    {%{a: "value"}, 0},
+    {true, 1},
+    {false, 2},
+    {5, 3},
+    {"a word", 4},
+    {6.4, 5},
+    {:a_thing, 6},
+    {[1,2,3], 7},
+    {{1,2}, 8},
   ]
 
   describe "tree_node?/1 guard" do
@@ -24,7 +39,7 @@ defmodule RoseTree.TreeNodeTest do
     end
 
     test "should return false when given bad values" do
-      for {value, idx} <- @bad_values do
+      for {value, idx} <- @bad_trees do
         assert TreeNode.tree_node?(value) == false,
                 "Expected `false` for element at index #{idx}"
       end
@@ -41,7 +56,7 @@ defmodule RoseTree.TreeNodeTest do
     end
 
     test "should return false when given bad values" do
-      for {value, idx} <- @bad_values do
+      for {value, idx} <- @bad_trees do
         assert TreeNode.empty?(value) == false,
                 "Expected `false` for element at index #{idx}"
       end
@@ -62,7 +77,7 @@ defmodule RoseTree.TreeNodeTest do
     end
 
     test "should return false when given bad values" do
-      for {value, idx} <- @bad_values do
+      for {value, idx} <- @bad_trees do
         assert TreeNode.leaf?(value) == false,
                 "Expected `false` for element at index #{idx}"
       end
@@ -83,9 +98,85 @@ defmodule RoseTree.TreeNodeTest do
     end
 
     test "should return false when given bad values" do
-      for {value, idx} <- @bad_values do
+      for {value, idx} <- @bad_trees do
         assert TreeNode.parent?(value) == false,
                 "Expected `false` for element at index #{idx}"
+      end
+    end
+  end
+
+  describe "empty/0" do
+    test "should return an empty TreeNode struct", %{empty_tree: tree} do
+      empty_tree = TreeNode.empty()
+
+      assert match?(tree, empty_tree)
+      assert TreeNode.empty?(empty_tree) == true
+      assert %TreeNode{term: nil, children: []} = empty_tree
+    end
+  end
+
+  describe "new/2 when using only the first parameter" do
+    test "should return a new leaf TreeNode for any valid erlang term besides nil" do
+      for {value, idx} <- @node_values do
+        new_tree = TreeNode.new(value)
+
+        assert TreeNode.leaf?(new_tree)
+        assert %TreeNode{term: ^value, children: []} = new_tree
+      end
+    end
+
+    test "should return a new empty TreeNode when nil is passed" do
+      new_tree = TreeNode.new(nil)
+
+      assert TreeNode.empty?(new_tree)
+      assert %TreeNode{term: nil, children: []} = new_tree
+    end
+  end
+
+  describe "new/2 when using both parameters" do
+    test "should return a new leaf TreeNode when the second arg is an empty list" do
+      new_tree = TreeNode.new(5, [])
+
+      assert TreeNode.leaf?(new_tree)
+      assert %TreeNode{term: 5, children: []} = new_tree
+    end
+
+    test "should return a new empty TreeNode when the first arg is nil and the second arg is an empty list" do
+      new_tree = TreeNode.new(nil, [])
+
+      assert TreeNode.empty?(new_tree)
+      assert %TreeNode{term: nil, children: []} = new_tree
+    end
+
+    test "should return a new parent TreeNode when the second arg is a populated list of values" do
+      new_tree = TreeNode.new(5, [4,3,2,1])
+
+      assert TreeNode.parent?(new_tree)
+    end
+
+    test "should have each input child turned into a new leaf TreeNode when the second arg is a populated list" do
+      input_list = [4,3,2,1]
+
+      expected_children = for x <- input_list, do: TreeNode.new(x)
+
+      new_tree = TreeNode.new(5, input_list)
+
+      assert %TreeNode{term: 5, children: ^expected_children} = new_tree
+
+      for child <- new_tree.children do
+        assert TreeNode.leaf?(child) == true
+      end
+    end
+
+    test "should return a new parent TreeNode with TreeNode children when the second arg is a populated list of TreeNodes" do
+      input_list = for x <- 1..4, do: TreeNode.new(x)
+
+      new_tree = TreeNode.new(5, input_list)
+
+      assert %TreeNode{term: 5, children: ^input_list} = new_tree
+
+      for child <- new_tree.children do
+        assert TreeNode.leaf?(child) == true
       end
     end
   end
