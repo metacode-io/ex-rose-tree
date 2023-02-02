00888e8ea3c3941a862a942471dba4508cda5fe9
StoatPower <mattcaldwell84@gmail.com>
2023-02-02 11:47:40 -0500

    unit test for Kin.great_grandparent/1

--- rose_tree/test/rose_tree/zipper/kin_test.exs
+++ rose_tree/test/rose_tree/zipper/kin_test.exs
@@ -13,9 +13,9 @@ defmodule RoseTree.Zipper.KinTest do
 
       expected = Context.from_locations([root_loc])
 
-      grandparent = Kin.grandparent(new_ctx)
+      actual = Kin.grandparent(new_ctx)
 
-      assert expected.focus.term == grandparent.focus.term
+      assert expected.focus.term == actual.focus.term
     end
 
     test "should return nil if the current Context's focus has no grandparent",
@@ -23,6 +23,26 @@ defmodule RoseTree.Zipper.KinTest do
       new_ctx = %Context{ctx | path: [root_loc]}
 
       assert Kin.grandparent(new_ctx) == nil
+    end
+  end
+
+  describe "great_grandparent/1" do
+    test "should return the great grandparent of the current Context's focus if it has one",
+         %{simple_ctx: ctx, root_loc: root_loc, loc_1: loc_1, loc_2: loc_2} do
+      new_ctx = %Context{ctx | path: [loc_2, loc_1, root_loc]}
+
+      expected = Context.from_locations([root_loc])
+
+      actual = Kin.great_grandparent(new_ctx)
+
+      assert expected.focus.term == actual.focus.term
+    end
+
+    test "should return nil if the current Context's focus has no great grandparent",
+         %{simple_ctx: ctx, root_loc: root_loc} do
+      new_ctx = %Context{ctx | path: [root_loc]}
+
+      assert Kin.great_grandparent(new_ctx) == nil
     end
   end
 end
